c5e9811f9bec8dc6dab7fc41c3168ebcb470730f
Daniel Marin Pacheco <damapa2003@gmail.com>
2025-04-17 11:07:03 +0200

    Eliminacion de print y agragacion de comentarios al codigo

--- Sonido el erizo/scripts/miscelaneous/gems.gd
+++ Sonido el erizo/scripts/miscelaneous/gems.gd
@@ -1,12 +1,13 @@
 extends Node2D
 
+# Obtiene el sprite animado cuando este listo y empezara la animacion por defecto
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
+# Al entrar un cuerpo en el area 2D empezara la animacion vanish
 func _on_area_2d_body_entered(_body: Node2D) -> void:
-	print("good kbron")
 	sprite.play("vanish")
 
-
+# Cuando termine una animacion si esta es vanish hara que la gema desaparezca de la escena
 func _on_animated_sprite_2d_animation_finished() -> void:
 	if sprite.animation == "vanish":
 		queue_free()
