2e3334e05f429c16d48b753e5385f5da88229bf0
Daniel Marin Pacheco <damapa2003@gmail.com>
2025-04-17 10:46:43 +0200

    Arreglo bumper

--- Sonido el erizo/scripts/miscelaneous/bumper.gd
+++ Sonido el erizo/scripts/miscelaneous/bumper.gd
@@ -1,20 +1,17 @@
 extends Node2D
 
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
-@export var character: CharacterBody2D
 
 var reversed = true
 var state_machine: StateMachine
 var air_state: State 
 
-func _ready() -> void:
-	state_machine = character.get_node("StateMachine")
-	air_state = state_machine.get_node("AirState")
-
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body == character:
+	state_machine = body.get_node("StateMachine")
+	air_state = state_machine.get_node("AirState")
+	if body is CharacterBody2D:
 		sprite.play("Activated")
-		character.velocity.y = -600
+		body.velocity.y = -600
 		reversed = false
 		
 		state_machine.switch_states(air_state)
