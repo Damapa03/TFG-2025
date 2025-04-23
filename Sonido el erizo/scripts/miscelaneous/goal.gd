be40119b2f5074bbb32d9c1b404577267147e374
Daniel Marin Pacheco <damapa2003@gmail.com>
2025-04-23 15:31:27 +0200

    Funciones base bandera goal (sin probar)

---
+++ Sonido el erizo/scripts/miscelaneous/goal.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+@onready var animatedSprite2D: AnimatedSprite2D = $AnimatedSprite2D
+@onready var timer: Timer = $Timer
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	animatedSprite2D.play("Spin")
+	timer.start
+
+
+func _on_timer_timeout() -> void:
+	animatedSprite2D.play("Finish")
